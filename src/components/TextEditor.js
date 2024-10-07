0996ec1e22d6656b4c39c1ec8c519e76f5d31096
Emil Folino <emilfolino@gmail.com>
2024-10-07 09:45:50 +0200

    Fixed socket updates

--- src/components/TextEditor.js
+++ src/components/TextEditor.js
@@ -1,8 +1,25 @@
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
+import { io } from "socket.io-client";
+
+const SERVER_URL = "http://localhost:8337";
 
 function TextEditor() {
     const [title, setTitle] = useState("");
     const [content, setContent] = useState("");
+
+    const socket = useRef(null);
+
+    useEffect(() => {
+      socket.current = io(SERVER_URL);
+
+      socket.current.on("content", (data) => {
+        setContent(data);
+      });
+
+      return () => {
+        socket.current.disconnect();
+      }
+    }, []);
 
     function clear(e) {
         e.preventDefault();
@@ -11,13 +28,19 @@ function TextEditor() {
         setContent("");
     }
 
+    function handleContentChange(e) {
+      const value = e.target.value;
+
+      socket.current.emit("content", value);
+    }
+
     return (
       <>
         <label htmlFor="title-field">Titel</label>
         <input type="text" id="title-field" name="title-field" value={title} onChange={(e) => setTitle(e.target.value)} />
 
         <label htmlFor="content-field">Innehåll</label>
-        <textarea id="content-field" value={content} onChange={(e) => setContent(e.target.value)}></textarea>
+        <textarea id="content-field" value={content} onChange={handleContentChange}></textarea>
 
         <button id="print-message" onClick={clear}>Rensa</button>
 
